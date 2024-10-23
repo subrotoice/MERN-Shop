8706021e735d581ceb47be138fc302c271d3e82c
promy <promyaaa@gmail.com>
2024-10-24 01:45:02 +0600

    user review model added and getProductsByCategory api added

--- server/app/controllers/categoryController.js
+++ server/app/controllers/categoryController.js
@@ -1,11 +1,23 @@
 // server/controllers/categoryController.js
 import Category from "../models/Category.js";
+import Product from "../models/Product.js";
 
 // Get all categories
 export const getAllCategories = async (req, res) => {
   try {
     const categories = await Category.find();
     res.status(200).json(categories);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get products by category
+export const getProductsByCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const products = await Product.find({ category: id })
+    res.status(200).json(products);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
